36c0f63785b0a3b6484e640d72466bddf7cca121
Julianmenav <julianmenav2@gmail.com>
2022-11-22 20:22:16 +0100

    client(style): Change login css

--- client/src/components/RoomForm.jsx
+++ client/src/components/RoomForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import chatIcon from "../assets/favicon.svg";
 
 const RoomForm = () => {
   const [room, setRoom] = useState("");
@@ -13,21 +14,25 @@ const RoomForm = () => {
   return (
     <div className="roomFormComponent">
       <div className="roomFormHeader"> 
-        <p>Join a Room</p>
+        <p>Join a Room <span><img src={chatIcon} alt=""></img></span></p>
       </div>
       <form className="roomForm" onSubmit={joinRoom}>
         <div id="inputs" >
+          <label for="name" >Name</label>
           <input
+            id="name"
             className="input"
             onChange={(event) => setName(event.target.value)}
-            placeholder="Enter your name"
+            placeholder="John Doe"
             required="required"
             pattern="[A-Za-z0-9]{1,10}[\s_-]?[A-Za-z0-9]{1,10}"
           />
+          <label for="room" >Room Number</label>
           <input
+            id="room"
             className="input"
             onChange={(event) => setRoom(event.target.value)}
-            placeholder="Enter a room number"
+            placeholder="4200"
             required="required"
             pattern="[0-9]{1,4}"
           />
