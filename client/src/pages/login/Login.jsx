d62550ff7a50d256b1f41400c8b0bad7849009b8
Julianmenav <julianmenav2@gmail.com>
2023-08-04 16:02:34 +0200

    style: responsive changes

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -32,12 +32,12 @@ function Login() {
   }
 
   return (
-    <section className="flex flex-col text-white w-full min-h-screen justify-center items-center">
+    <section className="flex flex-col text-white w-full min-h-screen justify-center items-center px-2">
       <div id="content" className="flex flex-col gap-12 items-center">
-        <p id="id" className="text-5xl font-bold break-after-page min-w-fit text-white bg-black px-12 py-8 rounded-full bg-opacity-10 text-center w-fit">
+        <p id="id" className="text-4xl px-6 py-4 md:text-5xl md:px-12 md:py-8 font-bold break-after-page min-w-fit text-white bg-black rounded-full bg-opacity-10 text-center w-fit">
           Web<span className="text-[#ADFF2F]">Sockets</span> Chat
         </p>
-        <div className="flex flex-col items-center w-96 max-w-sm bg-black bg-opacity-40 rounded-md py-4 px-6 gap-2">
+        <div className="flex flex-col items-center w-full max-w-sm bg-black bg-opacity-40 rounded-md py-4 px-6 gap-2">
           <p className="font-bold text-2xl">Enter a name (Guest)</p>
           <input id="name" type="text" onChange={(e) => setGuestName(e.target.value)} className="px-4 py-1.5 rounded-md bg-opacity-20 w-full bg-[#90e6cc] text-lg font-bold" />
           <button id="name" type="text" onClick={guestLogin} className="px-4 py-2 rounded-md  bg-[#6660ff] hover:bg-[#7c77ff] active:translate-y-[1px] font-bold text-center w-full" >Submit</button>
