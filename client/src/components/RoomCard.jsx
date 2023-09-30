c0ca6dbc3166301d3383d38c731ae7e662d39884
Julianmenav <julianmenav2@gmail.com>
2023-09-30 16:29:16 +0200

    fl0 server config

--- client/src/components/RoomCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function RoomCard({ fn, room }) {
-  return (
-    <div className="bg-[#45daad] bg-opacity-50 hover:bg-opacity-40 active:bg-opacity-20  sm:w-32 md:w-40 h-24 cursor-pointer rounded-xl p-2" onClick={fn}>
-      <span className="text-xl md:text-4xl font-bold opacity-60">{room}</span>
-    </div>
-  );
-}
